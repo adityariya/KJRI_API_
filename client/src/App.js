43fb5a43cdb3e90237ccaee5c1a45fa875789163
adityariya <aditya.ariya@yahoo.com>
2020-12-20 16:07:59 +0100

    Form

--- client/src/App.js
+++ client/src/App.js
@@ -2,37 +2,18 @@ import './App.css';
 import Header from './components/Header'
 import Footer from './components/Footer'
 import { Container } from 'react-bootstrap'
-import Form from './components/Form'
+import {Form} from './components/Form'
 
-var userIsRegistered = true;
 
-function renderConditionally(){
-  if (isLoggedIn === true){
-    return <h1>HALLO!</h1>:
-  }
-  else {
-    <form className="form">
-      <Input type="text"placeholder="Username" />
-      <Input type="password" placeholder="Password" />
-      <button type="submit"> Login </button>
-    </form>
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        <Form />
+      </div>
+    );
   }
 }
 
-function App() {
-  return (
-    <>
-      <Header/>
-      <main>
-          <Container>
-            <h2>Mau buat surat? </h2>
-            <Form isRegistered={userIsRegistered} />
-          </Container>
-         
-      </main>
-     <Footer/>
-    </>
-  );
-}
 
 export default App;
--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,20 +1,62 @@
-import React from "react";
-import Input from "./Input";
+import React, { Component } from 'react';
+import FormUserDetails from './FormUserDetails';
 
-function Form(props) {
-  return (
-    <form className="form">
-      <Input type="text"placeholder="Username" />
-      <Input type="password" placeholder="Password" />
-      <Input type="password" placeholder="Confirm Password" />
-
-      {!props.isRegistered && (
-        <input type="password" placeholder="Confirm Password" />
-      )}
-
-      <button type="submit">{props.isRegistered ? "Login" : "Register"}</button>
-    </form>
-  );
+export class Form extends Component {
+state ={
+  step: 1,
+  firstName: '',
+  lastName: '',
+  email: '',
+  jabatan:'',
 }
 
-export default Form;
+//next step
+  nextStep = () => {
+    const{ step } = this.state;
+    this.setState({
+      step: step + 1
+    });
+  }
+
+//back step
+prevStep = () => {
+  const{ step } = this.state;
+  this.setState({
+    step: step - 1
+  });
+}
+
+//Handle fields change
+handleChange = input => e => {
+  this.setState({[input]: e.target.value});
+
+}
+
+  render() {
+    const{ step } = this.state;
+    const{ firstName,lastName,email,jabatan } = this.state;
+    const values = { firstName,lastName,email,jabatan }
+    
+    switch(step) {
+      case 1:
+        return(
+          <FormUserDetails 
+            nextStep={this.nextStep}
+          handleChange={this.handleChange}
+          values={values}
+          />
+        )
+        case 2:
+          return<h1>FormPersonalDetails</h1>
+
+        case 3:
+          return<h1>Confirm</h1>
+
+        case 4:
+          return<h1>Success</h1>
+    }
+    
+  }
+}
+
+export default Form
