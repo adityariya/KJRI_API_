7c0c05c098a7171efbe563064c8e127c5d49b7cb
adityariya <aditya.ariya@yahoo.com>
2020-12-21 10:23:26 +0100

    form

--- client/src/components/FormUserDetails.js
+++ client/src/components/FormUserDetails.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import AppBar from 'material-ui/AppBar'
+import TextField from 'material-ui/TextField'
+import RaisedButton from 'material-ui/RaisedButton'
 
 export class FormUserDetails extends Component {
+
+    continue = e => {
+        e.preventDefault();
+        this.props.nextStep();
+}
+    
+
     render() {
+        const{ values, handleChange } = thins.props;
+        
         return (
-            <div>
-                <h1>Hello from</h1>
-            </div>
+            <MuiThemeProvider>
+                <React.Fragment>
+                    <AppBar title="Enter User Details"/>
+                    <TextField
+                    hintText="Masukan Nama Depan Anda"
+                    floatingLabelText="Nama Depan"
+                    onChange={handleChange('firstName')}
+                    />
+                </React.Fragment>
+            </MuiThemeProvider>
         )
     }
 }
